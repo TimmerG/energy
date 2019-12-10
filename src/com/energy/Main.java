b5ddf6cdbf7ab0d1f9397fb42cb3b69076b1b742
TimmerG <geert.timmermans08+1@gmail.com>
2019-12-10 10:54:33 +0100

    connection to db

--- src/com/energy/Main.java
+++ src/com/energy/Main.java
@@ -1,8 +1,29 @@
 package com.energy;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) throws Exception {
+	    //Todo write code here
+      get_Connection();
+    }
+
+    public static Connection get_Connection() throws Exception {
+      try {
+        String driver = "com.mysql.jdbc.Driver";
+        String url = "jdbc:mysql://localhost:3306/energy?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+        String username = "root";
+        String password = "root";
+        Class.forName(driver);
+
+        Connection conn = DriverManager.getConnection(url, username, password);
+        System.out.println("Connected");
+        return conn;
+      } catch (Exception e) {
+        System.out.println(e);
+      }
+      return null;
     }
 }
---
+++ src/com/energy/model/User.java
@@ -0,0 +1,16 @@
+package com.energy.model;
+
+import java.util.Date;
+import java.util.UUID;
+
+public class User {
+
+  private UUID id;
+  private String name;
+  private String city;
+  private String province;
+  private String country;
+
+  private Date createdAt;
+  private Date updatedAt;
+}
